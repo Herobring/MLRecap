a841b585d8b140487b66ac336e25a3d5650425b4
Vladimir Ershov <vladimir.ershov@clausematch.com>
2020-01-08 15:56:21 +0300

    Neuron implemented

---
+++ MLModels/neuron.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+
+class Neuron():
+
+    def __init__(self, weights: np.ndarray, b: float):
+        self.b = b
+        self.weights = weights
+
+    def apply(self, x: np.ndarray):
+        return np.dot(self.weights, x) + self.b
+
+    def update(self, weights: np.ndarray, b: float):
+        self.b = self.b + b
+        self.weights = np.add(self.weights, weights)
+
+    def __str__(self):
+        return "{{Neuron:{}+{}}}".format(self.weights, self.b)
+
+
+def base_test():
+    p = Neuron([1, 2, 3], 3)
+    if not isinstance(p, Neuron):
+        Exception('Created not a neuron')
+
+
+def apply_test():
+    p = Neuron([1, -1, 2, 0, 0], 100000)
+    res = p.apply([5, 10, 5, 343, 34343])
+
+    if res != 100000+5:
+        raise Exception(res)
+
+
+def update_test():
+    k = [1, -1, 2, 0, 0]
+    p = Neuron(k, 100000)
+    p.update([5, 10, 5, 0, 1], -99999)
+
+    if sum(np.equal(k, p.weights)) > 1:
+        raise Exception(p.weights)
+
+    if sum(np.not_equal(p.weights, [6, 9, 7, 0, 1])) > 0:
+        raise Exception(p.weights)
+
+    if p.b != 1:
+        raise Exception(p.b)
+
+
+def str_test():
+    p = Neuron([1, -1, 2, 0, 0], 100000)
+    if str(p) != '{Neuron:[1, -1, 2, 0, 0]+100000}':
+        raise Exception(p)
+
+
+def included_test():
+    base_test()
+    apply_test()
+    update_test()
+    str_test()
+
+
+if __name__ == '__main__':
+    included_test()
+    print()
+    print("Tests success!")
