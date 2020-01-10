3c186dd341d27e7250413a9ab605c61cff6bb09b
Vladimir Ershov <vladimir.ershov@clausematch.com>
2020-01-10 07:43:50 +0300

    LinReg fixed

--- MLModels/models.py
+++ MLModels/models.py
@@ -16,19 +16,21 @@ class Model:
 
 class LinearReg(Model):
 
-    def __init__(self, b: np.ndarray=None, e: np.ndarray=None):
+    def __init__(self, b: np.ndarray=None):
         self.b = b
-        self.e = e
 
     def fit(self, x: np.ndarray, y: np.ndarray)->str:
+        ons = np.ones((x.shape[0], 1))
+        x = np.concatenate((ons, x), axis=1)
         inv = np.linalg.inv(np.dot(np.transpose(x), x))
         residual_vec = np.dot(np.dot(inv, np.transpose(x)), y)
-        self.b = np.mean(residual_vec)
-        self.e = np.mean(y - np.dot(x, residual_vec))
-        return 'b={} e={}'.format(self.b, self.e)
+        self.b = residual_vec
+        return 'b={}'.format(self.b)
 
-    def predict(self, x: np.ndarray)->str:
-        return np.dot(x, self.b) + self.e
+    def predict(self, x: np.ndarray)->np.ndarray:
+        ons = np.ones((x.shape[0], 1))
+        x = np.concatenate((ons, x), axis=1)
+        return np.dot(x, self.b)
 
     def evaluate(self, x: np.ndarray, y: np.ndarray)->str:
         return str(np.square(self.predict(x) - y).mean(axis=None))
@@ -52,7 +54,7 @@ def generate(input=range(-10, 10), func=None):
 
 
 def model_test():
-    lm = LinearReg([4], [7])
+    lm = LinearReg([4])
 
     if not isinstance(lm, LinearReg):
         raise Exception(lm)
@@ -68,22 +70,20 @@ if __name__ == '__main__':
     print(generate())
     from visual import *
 
-    x = np.array(list(range(-100, 10, 1))).reshape(110, 1)
+    x = np.array(list(range(-10, 10, 1))).reshape(20, 1)
 
     def lin_func(x):
-        return -2*x + 30
+        return -2*x + 10
 
     y = generate(np.ndarray.flatten(x), lin_func)
     d1_plot(x, y)
     lr = LinearReg()
     print(lr.fit(x, y))
-    x2 = np.array(list(range(10, 20, 1))).reshape(10, 1)
+    x2 = np.array(list(range(10, 30, 1))).reshape(20, 1)
     y2 = generate(np.ndarray.flatten(x2), lin_func)
     print(lr.evaluate(x2,y2))
     print(lr.predict(x2))
-    print(lr.predict(y2))
     plt.plot(list(x) + list(x2), list(y) + list(y2))
     plt.plot(np.append(x, x2), np.append(lr.predict(x), lr.predict(x2)))
     plt.show()
-    plt.savefig(path)
 
